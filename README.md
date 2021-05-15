86126029a173ece4fbc84b66b8d0199eecd8d733
takumi000000 <taakumi.sabuakadesu@gmail.com>
2021-05-15 18:50:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 学校環境でのrails new
+
+dbはsqlite3
+
+寮でのスマホ接続
+bundle exec rails s -b 192.168.10.129
+# rails_sns
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'homes#index'
+  root 'posts#show'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
+end
